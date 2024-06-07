fef2e83fe5f77bafb28ed20b20a7b4cce4254588
Thiago Guidi <thiagoguidi@outlook.com>
2024-06-07 16:27:45 -0300

    Frontend configurations

---
+++ user-registration-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center bg-gradient-slate">
+            <h1 className="text-1xl font-Poppins">
+				Hello World with Vite, React and TailwindCss!!
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
--- user-registration-frontend/tailwind.config.js
+++ user-registration-frontend/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily:{
+                Poppins: ["Poppins", "Sans-serif"]
+            },
+            backgroundImage: {
+                'gradient-slate': 'linear-gradient(to bottom right, theme(colors.slate.900), theme(colors.slate.800), theme(colors.slate.700))'
+            }
+        }
+    },
+    plugins: [],
 }
 
